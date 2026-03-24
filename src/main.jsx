2aca73d93da3ca6cc86bc74fd2605a8d8fe70bfb
nishant-projects <nishantborse.098@gmail.com>
2026-03-24 10:24:03 +0530

    Add Lenis smooth scrolling setup in main entry

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
+import Lenis from 'lenis'
+import gsap from 'gsap'
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+const lenis = new Lenis({
+  duration: 1.4,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+})
+
+function raf(time) {
+  lenis.raf(time)
+  ScrollTrigger.update()
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+lenis.on('scroll', ScrollTrigger.update)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
